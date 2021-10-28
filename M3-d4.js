5f2c20be4b9e4071bde5d3c70149a34f450e26ca
Bilal Ahmad <bilal.ahmad0796@gmail.com>
2021-10-28 17:34:24 +0200

    change card background on click

--- M3-d4.js
+++ M3-d4.js
@@ -1,5 +1,5 @@
 //Promises
-function fetching(){
+
 fetch("https://striveschool-api.herokuapp.com/books")
 .then((data) => {
         return data.json()
@@ -12,32 +12,84 @@ fetch("https://striveschool-api.herokuapp.com/books")
         const col = document.createElement("div")
         col.className ="col-3"
         col.innerHTML = `
-        <div class="card" style="background.color:black;">
+        <div class="card" >
             <img src=${obj.img} class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${obj.title}</h5>
+                
+            <h5 class="title">${obj.title}</h5>
                 <div>
                 <p>${obj.category}</p>
                 <p>$${obj.price}</p>
                 </div>
-                <a href="#" onclick="additems()" class="btn btn-dark">add to cart</a>
+                <a href="#"  class="btn btn-dark">add to cart</a>
             </div>
         </div>
         `
         row.appendChild(col)
     }
 })
-.catch((error)=>console.log("Error catched",error))
-}
+.then(b=>{
+    let addbutton = document.querySelectorAll("a.btn")
+    
+    addbutton.forEach(btn=>{
+        btn.addEventListener('click' , function (e){
+            let titlearray=[]
+            let selectedtitle = btn.parentNode.querySelector("h5") //holdtitle
+            let tbodyRef = document.getElementById('myTable')
+            // Insert a row at the end of table
+            let newRow = tbodyRef.insertRow();
+            // Insert a cell at the end of the row
+            let newCell1 = newRow.insertCell();
+            let newCell2 = newRow.insertCell();
+            let newCell3 = newRow.insertCell();
+            let newCell4 = newRow.insertCell();
+            // Append a text node to the cell
+            let newText1 = document.createTextNode(selectedtitle);
+            newCell1.appendChild(newText1);
+            let newText2 = document.createTextNode('new row');
+            newCell2.appendChild(newText2);
+            let newText3 = document.createTextNode('new row');
+            newCell3.appendChild(newText3);
+            let newText4 = document.createTextNode('new row');
+            newCell4.appendChild(newText4);
+            
+            
+            
+            
+            let selectedcard = btn.parentNode
+            selectedcard.classList.add("bg-secondary")
+            titlearray.push(selectedtitle)
+            
+            console.log(titlearray)
+            
+        } )
+    }
+    )
 
-function additems(){
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
-}
+})
+.catch((error)=>console.log("Error catched",error))
+
+// function additems() {
+//     let tbodyRef = document.getElementById('myTable')
+//     // Insert a row at the end of table
+//     let newRow = tbodyRef.insertRow();
+//     // Insert a cell at the end of the row
+//     let newCell1 = newRow.insertCell();
+//     let newCell2 = newRow.insertCell();
+//     let newCell3 = newRow.insertCell();
+//     let newCell4 = newRow.insertCell();
+//     // Append a text node to the cell
+//     let newText1 = document.createTextNode('new row');
+//     newCell1.appendChild(newText1);
+//     let newText2 = document.createTextNode('new row');
+//     newCell2.appendChild(newText2);
+//     let newText3 = document.createTextNode('new row');
+//     newCell3.appendChild(newText3);
+//     let newText4 = document.createTextNode('new row');
+//     newCell4.appendChild(newText4);
+// }
+
+  
 //Callback functions
         // function greet(input){
         //     console.log("Hello",input)  
