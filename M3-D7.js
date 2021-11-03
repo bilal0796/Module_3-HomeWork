be0fd0b8aa135ae64556b8e0b5340796fa093698
Bilal Ahmad <bilal.ahmad0796@gmail.com>
2021-11-03 14:08:34 +0100

    Files added

--- M3-D7.js
+++ M3-D7.js
@@ -1,8 +1,34 @@
-const dataWithAsync = async () => {  
-    let response = await fetch('https://jsonplaceholder.typicode.com/users');  
-    let data = await response.json()    
-    return data
+// const dataWithAsync = async () => {  
+//     let response = await fetch('https://jsonplaceholder.typicode.com/users');  
+//     let data = await response.json()    
+//     return data
+// }
+
+
+// dataWithAsync().then(data => {
+//     const ul=document.getElementById("events")
+//     data.forEach(element => {
+//         const li=document.createElement("li")
+//         li.classList.add("list-group-item","d-flex", "justify-content-between")
+//         li.innerHTML=`<span>${element.email}</span> <span class="badge badge-dark">${element.website}</span>`
+//         ul.appendChild(li)
+//     });
+// })
+
+window.onload = async() => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        if (response.ok){
+            const events = await response.json()
+            const ul = document.getElementById("events")
+            events.forEach(element => {
+                const li = document.createElement("li")
+                li.classList.add("list.group-items")
+                li.innerHTML = `${element.name}${element.email}${element.website}`
+                ul.appendChild(li)
+            });
+        }
+    } catch(e){
+        alert(e)
+    }
 }
-
-
-dataWithAsync().then(data => console.log(data))
